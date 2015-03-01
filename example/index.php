ca1dd79444c8ceef013dc1dd084f03cd61a36b4b
Loïck Piera <pyrech@gmail.com>
2015-03-01 12:07:10 +0100

    Configure Box to build phar example making test easy on any platform

--- example/index.php
+++ example/index.php
@@ -17,6 +17,21 @@ use JoliNotif\Notifier;
 
 require __DIR__.'/../vendor/autoload.php';
 
+$icon = __DIR__.'/notification-icon.png';
+
+// This part make the icon accessible when JoliNotif is used inside a phar
+if (0 === strpos($icon, 'phar://')) {
+    $pharUri          = dirname(dirname(__FILE__));
+    $pharPath         = str_replace('phar://', '', $pharUri);
+    $iconRelativePath = substr($icon, strlen($pharUri)+1);
+    $tmpDir           = sys_get_temp_dir().'/jolinotif';
+
+    $phar = new Phar($pharPath);
+    $phar->extractTo($tmpDir, $iconRelativePath, true);
+
+    $icon = $tmpDir.'/'.$iconRelativePath;
+}
+
 $notifier = new Notifier([
     new TerminalNotifierDriver(),
     new GrowlNotifyDriver(),
@@ -26,6 +41,6 @@ $notifier = new Notifier([
 $notification = new Notification();
 $notification->setTitle('I\'m a notification title');
 $notification->setBody('And this is the body');
-$notification->setIcon(__DIR__.'/notification-icon.png');
+$notification->setIcon($icon);
 
 $notifier->send($notification);
