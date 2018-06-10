919314e0719bfdcea5a69e18c236d390723a6480
darkterminal <fscodexxx@yahoo.com>
2018-06-10 14:39:03 +0700

    Add more example using ReactPHP and JoliNotif

---
+++ example/notifyme/README.md
@@ -0,0 +1,7 @@
+# Usage
+
+1. Paste this `example`
+2. Install dependencies using Composer `composer install`
+3. After installing dependencies, type in your terminal / command prompt `php notify.php`
+4. Then open your browser and type in URL : `http://127.0.0.1:8181`
+5. Done!
---
+++ example/notifyme/notify.php
@@ -0,0 +1,60 @@
+<?php 
+
+use Joli\JoliNotif\Notification;
+use Joli\JoliNotif\NotifierFactory;
+use Psr\Http\Message\ServerRequestInterface;
+use React\Http\Response;
+
+require_once 'vendor/autoload.php';
+
+// create EventLoop
+$loop = React\EventLoop\Factory::create();
+
+// ReactPHP HTTP Server
+$server = new \React\Http\Server([
+    function (ServerRequestInterface $request) {
+
+    	$path = $request->getUri()->getPath();
+	   	$method = $request->getMethod();
+	   	$requestTarget = $request->getRequestTarget();
+    	
+        // Get data from Parsed in Body Fields
+        $data = $request->getParsedBody();
+
+        if ($path = '/') {
+
+        	switch ($method) {
+        		case 'GET':
+        			$response = "This Using ReactPHP EventLoop and Http!";
+        			// Create a Notifier
+        			$notifier = NotifierFactory::create();
+
+        			// Create your notification
+        			$notification =
+        			    (new Notification())
+        			    ->setTitle('Notification From WebApp')
+        			    ->setBody('This is cool right?')
+        			    // ->setIcon(__DIR__.'/path/to/your/icon.png')
+        			    // ->addOption('subtitle', 'This is a subtitle') // Only works on macOS (AppleScriptNotifier)
+        			    // ->addOption('sound', 'Frog') // Only works on macOS (AppleScriptNotifier)
+        			;
+
+        			// Send it
+        			$notifier->send($notification);
+        			break;
+        		case 'POST':
+        			$response = "This Using ReactPHP EventLoop and Http!";
+        			break;
+        	}
+        	return new Response(200, ['Content-Type' => 'text/plain'],  $response);
+        }
+
+    }
+]);
+
+$socket = new \React\Socket\Server('127.0.0.1:8181', $loop);
+$server->listen($socket);
+
+echo 'Listening on ' . str_replace('tcp:', 'http:', $socket->getAddress()) . "\n";
+
+$loop->run();
